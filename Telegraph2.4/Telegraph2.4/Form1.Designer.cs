cc35da7f1353aef3037c7e57df44d76699ed6f44
Babak <111429093+BabakStars@users.noreply.github.com>
2023-12-20 14:38:53 +0330

    Add files via upload

---
+++ Telegraph2.4/Telegraph2.4/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Telegraph2._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Channel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(246, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Address";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(482, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(299, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ports";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "BaudRate: 115200";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(98, 57);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 37);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Close";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.textBox3);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox4.Location = new System.Drawing.Point(13, 118);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(768, 112);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Transmitter";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(684, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 84);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 21);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(672, 84);
+            this.textBox3.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button5);
+            this.groupBox5.Controls.Add(this.textBox4);
+            this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox5.Location = new System.Drawing.Point(13, 246);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(768, 121);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Receiver";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(684, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 94);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 21);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(672, 94);
+            this.textBox4.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(793, 379);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Babak Stars";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Telegraph2.4/Telegraph2.4/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace Telegraph2._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int check = 0;
+        string StrRecieve;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            if (check == 0)
+            {
+                MessageBox.Show("ERROR 8: Check Your connection.", "Something went wrong");
+                return;
+            }
+
+            if (text.Length == 0 || text.Length > 3)
+            {
+                MessageBox.Show("ERROR 0: You cannot enter this value.", "Something went wrong");
+                return;
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] < 48 || text[i] > 57)
+                {
+                    MessageBox.Show("ERROR 1: You cannot enter this value.", "Something went wrong");
+                    return;
+                }
+            }
+
+            if (Convert.ToInt16(text) < 0 || Convert.ToInt16(text) > 125)
+            {
+                MessageBox.Show("ERROR 2: The value must be between 0 and 125.", "Something went wrong");
+                return;
+            }
+
+            serialPort1.WriteLine("@" + textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 115200;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            String[] ports = SerialPort.GetPortNames();
+
+            button6.Enabled = false;
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.IndexOf("^") != -1 || textBox3.Text.IndexOf("@") != -1 || textBox3.Text.IndexOf("#") != -1)
+            {
+                MessageBox.Show("ERROR 8: The text contains invalid characters. ^#@", "Something went wrong");
+                return;
+            }
+            if (textBox3.Text.Length < 1)
+            {
+                MessageBox.Show("ERROR 1: You cannot enter this value.", "Something went wrong");
+                return;
+            }
+            if (check == 0)
+            {
+                MessageBox.Show("ERROR 8: Check Your connection.", "Something went wrong");
+                return;
+            }
+
+            serialPort1.WriteLine("^" + textBox3.Text.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            {
+                String portesr = comboBox1.Text.ToString();
+                serialPort1.PortName = portesr;
+                check = 1;
+                serialPort1.Open();
+                button6.Enabled = true;
+                button3.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("ERROR 8: Check Your connection.", "Something went wrong");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text2 = textBox2.Text;
+
+            if (check == 0)
+            {
+                MessageBox.Show("ERROR 8: Check Your connection.", "Something went wrong");
+                return;
+            }
+
+            if (text2.Length != 5)
+            {
+                MessageBox.Show("ERROR 0: You cannot enter this value.", "Something went wrong");
+                return;
+            }
+            else
+            {
+                for (int j = 0; j < text2.Length; j++)
+                {
+                    if (text2[j] < 48 || text2[j] > 57)
+                    {
+                        MessageBox.Show("ERROR 1: You cannot enter this value.", "Something went wrong");
+                        return;
+                    }
+                }
+            }
+            serialPort1.WriteLine("#" + textBox2.Text);
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            //textBox4.AppendText(StrRecieve);
+            textBox4.Text = StrRecieve;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            StrRecieve = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(DisplayText));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                button6.Enabled = false;
+                button3.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+    }
+}
